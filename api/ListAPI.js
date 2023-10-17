5f82495f6c5cfdb3b4a99a853ad3866ff015bcc1
vigneshwaran48 <p3487260@gmail.com>
2023-10-17 21:11:44 +0530

    Added csrf token in list post request

--- api/ListAPI.js
+++ api/ListAPI.js
@@ -1,18 +1,38 @@
+import Cookies from "js-cookie";
 import { ServerAPIManager } from "../utility/ServerAPIManager"
+
+const sendRequestWithCsrf = async (url, method, includeBody, body) => {
+    const csrfToken = Cookies.get("XSRF-TOKEN");
+    if(includeBody) {
+        const response = await fetch(url, {
+            method,
+            headers: {
+                "Content-Type": "application/json",
+                "X-XSRF-TOKEN": csrfToken
+            },
+            body
+        });
+        return await response.json();
+    }
+    else {
+        const response = await fetch(url, {
+            method,
+            headers: {
+                "Content-Type": "application/json",
+                "X-XSRF-TOKEN": csrfToken
+            }
+        });
+        return await response.json();
+    }
+}
 
 export const ListAPI = {
 
     addList: async listDetails => {
 
         const url = ServerAPIManager.ServerURL + ServerAPIManager.getAppRoutes().list.base;
-        const response = await fetch(url, {
-                                    method: "POST",
-                                    headers: {
-                                        "Content-Type": "application/json"
-                                    },
-                                    body: JSON.stringify(listDetails)
-                                });
-        return await response.json();
+
+        return await sendRequestWithCsrf(url, "POST", true, JSON.stringify(listDetails));
     },
     getListById: async listId => {
 
